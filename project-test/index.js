d7a8f6b56de156a2e5257df2cf0a83202d2acd4b
syarifabdillah <sectenzorg@gmail.com>
2022-10-18 09:14:48 +0700

    fix name

--- project-test/index.js
+++ project-test/index.js
@@ -4,27 +4,27 @@ const port = 3000
  
 server.get('/hiji', (req, res) => {
   console.log(req.headers)
-  res.status(200).send('API v1') 
+  res.status(200).send('API v1 (HIJI)') 
 })
 
 server.get('/dua', (req, res) => {
   console.log(req.headers)
-  res.status(200).send('API v2')
+  res.status(200).send('API v2 (DUA)')
 })
 
 server.get('/tilu', (req, res) => {
   console.log(req.headers)
-  res.status(200).send('API V3')
+  res.status(200).send('API v3 (TILU)')
 })
 
 server.get('/opat', (req, res) => {
     console.log(req.headers)
-    res.status(200).send('API V4')
+    res.status(200).send('API v4 (OPAT)')
 })
 
 server.get('/lima', (req, res) => {
     console.log(req.headers)
-    res.status(200).send('API V5')
+    res.status(200).send('API v5 (LIMA)')
 })
 
 server.listen(port, () => {
